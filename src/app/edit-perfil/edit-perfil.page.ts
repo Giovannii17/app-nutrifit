a787a8f32bc082dce9a5cdc4958e353438db678f
Giovannii17 <giovannisilvasantos173@gmail.com>
2024-11-12 15:46:01 -0300

    Correções e Melhorias

--- src/app/edit-perfil/edit-perfil.page.ts
+++ src/app/edit-perfil/edit-perfil.page.ts
@@ -6,10 +6,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-perfil.page.scss'],
 })
 export class EditPerfilPage implements OnInit {
+  nome: string = '';
+  altura: string = '';
+  dataNascimento: string = '';
+  sexo: string = '';
+  objetivo: string = '';
+  nivelTreino: string = '';
+  nivelAtividade: string = '';
+  isFormValid: boolean = false;
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  // Função para selecionar apenas um nível de treino
+  selectNivelTreino(value: string) {
+    this.nivelTreino = value;
+    this.validateForm();
   }
 
+  // Função para selecionar apenas um nível de atividade
+  selectNivelAtividade(value: string) {
+    this.nivelAtividade = value;
+    this.validateForm();
+  }
+
+  // Função para validar o formulário
+  validateForm() {
+    this.isFormValid = this.nome && this.altura && this.dataNascimento && this.sexo &&
+                       this.objetivo && this.nivelTreino && this.nivelAtividade ? true : false;
+  }
+
+  // Função para salvar o perfil
+  salvarPerfil() {
+    if (this.isFormValid) {
+      // Lógica para salvar o perfil
+      console.log('Perfil salvo com sucesso!');
+    } else {
+      console.log('Preencha todos os campos antes de salvar.');
+    }
+  }
 }
